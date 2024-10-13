8797e2b3494b953dc050482ac35cb32388b875ff
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-10-13 23:29:51 +0200

    fourth commit

--- variables.js
+++ variables.js
@@ -2,3 +2,17 @@ console.log("30DaysofJS"); // string
 console.log(false); // boolean
 console.log(undefined); // undefined
 console.log(null); // null
+
+let firstName = "Mauro",
+  lastName = "Icardi",
+  maritalStatus = "married",
+  country = "Argentina",
+  age = 30;
+
+console.log(firstName, lastName, maritalStatus, country, age);
+
+const myAge = "I'm 25 years old.";
+const yourAge = "You're 30 years old.";
+
+console.log(myAge);
+console.log(yourAge);
