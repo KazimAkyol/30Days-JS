5630d4af1c0fe692918b7d8847eecf5973410a86
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-10-13 22:33:15 +0200

    second commit

--- introduction.js
+++ introduction.js
@@ -1,1 +1,7 @@
-console.log("bu yazi introduction js den geldi");
+// console.log("bu yazi introduction js den geldi");
+
+console.log(typeof "Prototurk"); // string
+console.log(typeof 5); // number
+console.log(typeof true); // boolean
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
