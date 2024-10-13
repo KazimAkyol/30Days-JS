f7ee8b324c65d9598c5cb375af1b4ce66ce0437d
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-10-13 22:56:46 +0200

    third commit

--- introduction.js
+++ introduction.js
@@ -5,3 +5,30 @@ console.log(typeof 5); // number
 console.log(typeof true); // boolean
 console.log(typeof null); // object
 console.log(typeof undefined); // undefined
+
+let youtubeChannel = "prototurk",
+  channelAge = 11,
+  isChannelStillActive = true,
+  secondChannel = null,
+  channelMoney = undefined;
+
+console.log(
+  youtubeChannel,
+  channelAge,
+  isChannelStillActive,
+  secondChannel,
+  channelMoney
+);
+
+const PI = 3.14;
+const gravity = 9.81;
+
+console.log(gravity, PI);
+
+
+// Comments can make code readable
+// I learn to 30DaysofJavaScript(13.10.2024,Sunday)
+
+/* Comments can make readable,
+easy to reuse and informative */
+
---
+++ variables.js
@@ -0,0 +1,4 @@
+console.log("30DaysofJS"); // string
+console.log(false); // boolean
+console.log(undefined); // undefined
+console.log(null); // null
