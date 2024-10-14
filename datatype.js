c6e1cc0011c7f9a54e2c30ec5259098ba437b0b0
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-10-14 20:12:06 +0200

    fifth commit

--- datatype.js
+++
@@ -1,5 +0,0 @@
-console.log(typeof 91); // typeof number
-console.log(typeof false); // typeof boolean
-console.log(typeof "245"); // typeof string
-console.log(typeof undefined); // typeof undefined
-console.log(typeof null); // typeof object
